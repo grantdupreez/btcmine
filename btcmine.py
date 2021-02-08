b0d5fd11c60cd5a41101e97cfe9037d735f7fb86
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-02-08 21:44:07 +0000

    Update btcmine.py

--- btcmine.py
+++ btcmine.py
@@ -21,7 +21,7 @@ if __name__=='__main__':
     Dhaval->Bhavin->20,
     Mando->Cara->45
     '''
-    difficulty=6 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
+    difficulty=7 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
     import time
     start = time.time()
     st.write("start mining")
